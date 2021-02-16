da501e83e18a1c4c6aa096f03d7402eaa2fece18
whenes <whenes.oliveira@gmail.com>
2021-02-16 14:39:13 -0300

    Project developed during the Rocketseat Omnistqck10 week

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
+
+import Routes from './src/routes';
+
+YellowBox.ignoreWarnings([
+  'Unreconized WebSocket'
+]);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7d40e7'/>
+      <Routes/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+function Profile({ navigation }) {
+  const gitHubUserName = navigation.getParam('github_username');
+
+return <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${gitHubUserName}` }} />;
+}
+
+export default Profile;
